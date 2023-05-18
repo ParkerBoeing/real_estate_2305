7912e7d467ec731cc380c844ae334b986709c3a5
Parker Boeing <parker.boeing@gmail.com>
2023-05-18 10:16:21 -0600

    Feat / Test: add method and test for discerning total area of rooms in house

--- lib/house.rb
+++ lib/house.rb
@@ -10,4 +10,24 @@ class House
   def add_room(room)
     @rooms << room
   end
+
+  def above_market_average?
+    if @price > 500000
+      return true
+    else
+      return false
+    end
+  end
+
+  def rooms_from_category(category)
+    rooms.find_all {|room| room.category == category}
+  end
+
+  def area
+    total_area = 0
+    rooms.each do |room| 
+      total_area += room.area
+    end
+    total_area
+  end
 end
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -35,4 +35,54 @@ RSpec.describe Room do
       expect(house.rooms).to eq([room_1, room_2])
     end
   end
+  
+  describe "Iteration 3" do
+    
+    it "can return false if the house is not above market average" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.above_market_average?).to eq(false)
+    end
+
+    it "can return true if the market is above market average" do
+      house = House.new("$750000", "123 sugar lane")
+      expect(house.above_market_average?).to eq(true)
+    end
+
+    it "can separate rooms by category" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      expect(house.rooms_from_category(:bedroom)).to eq([room_1, room_2])
+      expect(house.rooms_from_category(:basement)).to eq([room_4])
+    end
+
+    it "can determine the area of all the rooms in the house" do
+      house = House.new("$400000", "123 sugar lane")
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      expect(house.area).to eq(1900)
+    end
+
+    xit "can return the details of the house as a hash" do
+      house = House.new("$400000", "123 sugar lane")
+      expected = {
+        price: 400000
+        address: "123 sugar lane"
+      }
+      expect(house.details).to eq(expected)
+    end
+  end
 end
