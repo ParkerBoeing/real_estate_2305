7b665ba254d525662682574b9ef82ce8df6fb9bc
Parker Boeing <parker.boeing@gmail.com>
2023-05-18 09:36:57 -0600

    Feat / Test: add a room class and spec

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,25 @@
 class Room
+attr_reader :category, :length, :width
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+    @is_painted = false
+  end
 
-  def initialize
+  def area
+    length * width.to_i
+  end
 
+  def paint
+    @is_painted = true
+  end
+
+  def is_painted?
+    if @is_painted == false
+      return false
+    else
+      return true
+    end
   end
 end
